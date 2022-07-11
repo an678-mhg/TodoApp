58f39faf6b29c556bcbe1642acb15f48b3847b41
Nguyễn Quốc An <79411491+an678-mhg@users.noreply.github.com>
2022-07-11 10:26:41 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="bg-black h-[100vh] flex items-center justify-center flex-col">
       <h1 className="text-[30px] text-white font-[600] mb-6">
-        Todo App CodeGym!
+        Todo App
       </h1>
       <TodoListContainer />
     </div>
